e3874e46fa990df69df7e496cd47e67863490044
Clemoss <lemossc@oregonstate.edu>
2019-12-11 13:09:46 -0800

    Merged modal-functionality with master

--- public/budget.js
+++ public/budget.js
@@ -1,34 +1,4 @@
-// Budget page behavior
-document.addEventListener("DOMContentLoaded", function() {
-  //get modals
-  var modal = document.getElementsByClassName('modal-dialog');
-  //get buttons that open modals
-  var btn = document.getElementsByClassName('arrow-right');
-  //get buttons that close modals
-  var next = document.getElementsByClassName('modal-close')
-
-  //When window loads, set first modal to display
-  window.onload = function() {
-    modal[0].hidden = false;
-    modal[1].style.zIndex = 1011;
-    modal[0].style.zIndex = 1012;
-    modal[1].hidden = true;
-  }
-
-  //When next button is pressed, activate next modal
-  document.querySelector('.arrow-right').addEventListener('click', () => {
-    console.log("Button clicked");
-    modal[0].hidden = true;
-    modal[0].style.zIndex = 1011;
-    modal[1].hidden = false;
-    modal[1].style.zIndex = 1012;
-  });
-});
-
-
-
 // INCOME TAX
-<<<<<<< HEAD
 // var incomeTaxData = require('./incomeTaxData');
 
 // function getState() {
@@ -87,66 +57,6 @@ document.addEventListener("DOMContentLoaded", function() {
     
 //     return incomeTax;
 // }
-=======
-var incomeTaxData = require('./incomeTaxData');
-
-function getState() {
-    return 'Oregon';
-}
-
-function getIncome() {
-    return 120000;
-}
-
-function getFilingStatus() {
-    return 'single';
-}
-
-function calculateIncomeTax(state, initialIncome, filingStatus) {
-    console.log('');
-    var incomeTax = 0;
-    if (filingStatus == 'single') {
-        for (var i = 0; incomeTaxData[state].brackets.single[i] <= initialIncome; i++) {
-            var currentBracket = incomeTaxData[state].brackets.single[i];
-            console.log(' = currentBracket: $' + currentBracket);
-
-            var nextBracket = incomeTaxData[state].brackets.single[i + 1];
-            console.log('   nextBracket: $' + nextBracket);
-
-            var rate = incomeTaxData[state].rates[i] / 100;
-            console.log('   rate: ' + rate);
-
-            if (nextBracket && initialIncome > nextBracket) {
-                incomeTax = incomeTax + ((nextBracket - currentBracket - 1) * rate);
-            } else {
-                incomeTax = incomeTax + ((initialIncome - currentBracket) * rate);
-            }
-            console.log('   incomeTax: $' + incomeTax);
-        }
-    } else if (filingStatus == 'marriedJointly') {
-        for (var i = 0; incomeTaxData[state].brackets.marriedJointly[i] <= initialIncome; i++) {
-            var currentBracket = incomeTaxData[state].brackets.marriedJointly[i];
-            console.log(' = currentBracket: $' + currentBracket);
-
-            var nextBracket = incomeTaxData[state].brackets.marriedJointly[i + 1];
-            console.log('   nextBracket: $' + nextBracket);
-
-            var rate = incomeTaxData[state].rates[i] / 100;
-            console.log('   rate: ' + rate);
-
-            if (nextBracket && initialIncome > nextBracket) {
-                incomeTax = incomeTax + ((nextBracket - currentBracket - 1) * rate);
-            } else {
-                incomeTax = incomeTax + ((initialIncome - currentBracket) * rate);
-            }
-            console.log('   incomeTax: $' + incomeTax);
-        }
-    }
-    console.log('');
-
-    return incomeTax;
-}
->>>>>>> 3cb0a5d535d69a17f4a641998d9409c6881c37fa
 
 // var state = getState();
 
@@ -239,23 +149,3 @@ for (var i = 0; i < subscriptions.length; i++) {
 }
 
 var annualExpenses = monthlyExpenses * 12;
-<<<<<<< HEAD
-
-
-
-// MODAL
-var modalBackdrop = document.getElementById('modal-backdrop');
-var modal = document.getElementById('prompt-modal');
-
-var startButton = document.getElementById('start-button');
-startButton.addEventListener('click', function(event) {
-    toggleModal();
-    console.log('Start button was clicked');
-});
- 
-function toggleModal() {
-    modal.classList.toggle('hidden');
-    modalBackdrop.classList.toggle('hidden');
-}
-=======
->>>>>>> 3cb0a5d535d69a17f4a641998d9409c6881c37fa
