c0038c1fe2d7c32d7f564c20cb19d3fca7f7d366
Clemoss <lemossc@oregonstate.edu>
2019-12-11 01:07:58 -0800

    Added server handling for the modal prompts (/budget/:promptNumber)

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var expressHandlebars = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
-// var prompts = require('./prompts')
+var prompts = require('./prompts')
 
 app.engine('handlebars', expressHandlebars( { defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res, next) {
     res.status(200).render('index');
@@ -15,9 +17,15 @@ app.get('/', function(req, res, next) {
 
 app.get('/budget', function(req, res, next) {
     res.status(200).render('budget');
-})
+});
 
-app.use(express.static('public'));
+app.get('/budget/:promptNumber', function(req, res, next) {
+    if (req.params.promptNumber >= 1 && req.params.promptNumber <= prompts.length) {
+        res.status(200).render('./partials/modal', prompts[req.params.promptNumber]);
+    } else {
+        next();
+    }
+});
 
 app.get('*', function(req, res) {
     res.status(404).render('404');
