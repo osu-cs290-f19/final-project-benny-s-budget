11294e156f399aabc6178f8545def2496b487d76
Richard Villagrana <villagri@flip3.engr.oregonstate.edu>
2019-12-11 19:41:04 -0800

    Added A function that'll pass user input to another text box Missing "what is the document ID name for ('')

--- public/budget.js
+++ public/budget.js
@@ -204,3 +204,133 @@ function explodePie (e) {
     e.chart.render();
 
 }
+
+/*Passing Variables to other text boxes*/
+
+/*Income*/
+var plug_income = document.getElementById('income-input');
+var plug_otIncome = document.getElementById('other-income');
+/*Tax Information*/
+var plug_state = document.getElementById('state-input');
+var plug_status = document.getElementById('filling-status-input');
+/*Living Expenses*/
+var plug_housing = document.getElementById('housing-input');
+var plug_food = document.getElementById('food-input');
+var plug_medicine = document.getElementById('medicine-input');
+var plug_health = document.getElementById('health-insurance-input');
+var plug_utilities = document.getElementById('utilities-input');
+var plug_internet = document.getElementById('internet-input');
+var plug_phone = document.getElementById('phone-input');
+/*LifeStyle*/
+var plug_card = document.getElementById('credit-card-payments-input');
+var plug_house = document.getElementById('house-supplies-input');
+var plug_clothing = document.getElementById('clothing-input');
+var plug_laundry = document.getElementById('laundry-input');
+var plug_rec = document.getElementById('recreation-input');
+var plug_subs = document.getElementById('subscriptions-input');
+/*Transportation*/
+var plug_cPay = document.getElementById('car-payment-input');
+var plug_cInsure = document.getElementById('car-insurance-input');
+var plug_cRep = document.getElementById('car-repairs-input');
+var plug_gas = document.getElementById('gas-input');
+var plug_pubTrans = document.getElementById('public-transportation-input');
+var plug_vaK = document.getElementById('vacation-input');
+/*Student*/
+var plug_tuition = document.getElementById('tuition-input');
+var plug_fees = document.getElementById('fees-input');
+var plug_books = document.getElementById('text-books-input');
+/*Other Expenses*/
+var plug_sloan = document.getElementById('student-loan-input');
+var plug_ccare = document.getElementById('child-care-input');
+var plug_eFunds = document.getElementById('emergency-fund-input');
+var plug_savings = document.getElementById('savings-input');
+var plug_oExpen = document.getElementById('other-expenses-input');
+
+/*  */
+/*Income*/
+var results_income = document.getElementById('');
+var results_otIn = document.getElementById('');
+/*Tax Information*/
+var results_state = document.getElementById('');
+var results_status = document.getElementById('');
+/*Living Expenses*/
+var results_housing = document.getElementById('');
+var results_food = document.getElementById('');
+var results_medicine = document.getElementById('');
+var results_health= document.getElementById('');
+var results_utilities = document.getElementById('');
+var results_internet = document.getElementById('');
+var results_phone = document.getElementById('');
+/*LifeStyle*/
+var results_card = document.getElementById('');
+var results_house = document.getElementById('');
+var results_clothing = document.getElementById('');
+var results_laundry = document.getElementById('');
+var results_rec = document.getElementById('');
+var results_subs = document.getElementById('');
+/*Transportation*/
+var results_cPay = document.getElementById('');
+var results_cInsure = document.getElementById('');
+var results_cRep = document.getElementById('');
+var results_gas = document.getElementById('');
+var results_pubTrans = document.getElementById('');
+var results_vaK = document.getElementById('');
+/*Student*/
+var results_tuition = document.getElementById('');
+var results_fees = document.getElementById('');
+var results_books = document.getElementById('');
+/*Other Expenses*/
+var results_sloan = document.getElementById('');
+var results_ccare = document.getElementById('');
+var results_eFunds = document.getElementById('');
+var results_saving = document.getElementById('');
+var results_oExpen = document.getElementById('');
+/*
+ * set_vaule function
+ * When called will grab the user input and store it to the the interactable form 
+ * at the end
+ *
+ */
+function  set_value(){
+
+	/*Other Expenses*/
+/*Income*/
+results_income	 	= plug_income.value
+results_otIn	 	= plug_otIn.value 		
+/*Tax Information*/       
+results_state	 	= plug_state.value 	
+results_status	 	= plug_status.value 	
+/*Living Expenses*/      
+results_housing	 	= plug_housing.value 	
+results_food 	 	= plug_food.value 		
+results_medicine 	= plug_medicine.value 	
+results_health		= plug_health.value 	
+results_utilities 	= plug_utilities.value 	
+results_internet 	= plug_internet.value 	
+results_phone 		= plug_phone.value 	
+/*LifeStyle*/
+results_card 		= plug_card.value 		
+results_house 		= plug_house.value 	
+results_clothing 	= plug_clothing.value 	
+results_laundry 	= plug_laundry.value 	
+results_rec 		= plug_rec.value 		
+results_subs 		= plug_subs.value 		
+/*Transportation*/
+results_cPay 		= plug_cPay.value 		
+results_cInsure 	= plug_cInsure.value 	
+results_cRep 		= plug_cRep.value 		
+results_gas 		= plug_gas.value
+results_pubTrans 	= plug_pubTrans.value 
+results_vaK 		= plug_vaK.value 
+/*Student*/
+results_tuition 	= plug_tuition.value
+results_fees 		= plug_fees.value
+results_books 		= plug_books.value
+/*Other Expenses*/
+results_sloan.value  = plug_sloan.value;
+results_ccare.value  = plug_ccare.value;
+results_eFunds.value = plug_eFunds.value;
+results_saving.value = plug_saving.value;
+results_oExpen.value = plug_oExpen.value;
+
+}
